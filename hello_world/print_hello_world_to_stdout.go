bf2e16d2392407b9dcf26686250f80bb92f78e74
Bigizic <olalekanisaac75@gmail.com>
2026-06-01 00:17:31 +0100

    add updates to first dir

--- hello_world/print_hello_world_to_stdout.go
+++ hello_world/print_hello_world_to_stdout.go
@@ -1,6 +1,10 @@
 package main
+
 import "fmt"
 
+/**
+* main - simple function that prints hello world to stdout
+ */
 func main() {
-    fmt.Println("Hello, World")
+	fmt.Println("Hello, World")
 }
